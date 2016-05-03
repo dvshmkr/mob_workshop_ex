3adc7436407fe74c3512ff4ea0d667617ff5cb63
Sean Olson <nomdegoo@gmail.com>
2016-05-02 19:00:58 -0700

    added mob testing shell

---
+++ js/index.js
@@ -0,0 +1,26 @@
+Mob = (function(){
+    var api = {};
+
+    api.prob_1 = function(inpt){
+        var result = '';
+        //your brilliant ideas go here
+        console.log(result);
+    };
+    api.prob_2 = function(inpt){
+        var result = '';
+        //your brilliant ideas go here
+        console.log(result);
+    };
+    api.prob_3 = function(inpt){
+        var result = '';
+        //your brilliant ideas go here
+        console.log(result);
+    };
+    api.prob_4 = function(inpt){
+        var result = '';
+        //your brilliant ideas go here
+        console.log(result);
+    };
+
+    return api
+})();
